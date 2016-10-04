626772328962c7a722e3cfb40a7b31102299f1b1
sherlock1982 <nick.orekhov@gmail.com>
2016-10-04 12:14:35 +0300

    added gss-spnego

---
+++ Mechanisms/SaslGssSpnego.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace S22.Sasl.Mechanisms
+{
+    public class SaslGssSpnego : SaslNtlmv2
+    {
+        public SaslGssSpnego(NetworkCredential credential)
+            : base(credential)
+        {
+
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "GSS-SPNEGO";
+            }
+        }
+    }
+}
